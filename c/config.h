c8512e490a349c8beaa26b674715fb071ae48a8b
Peter <30462819+ptrpfa@users.noreply.github.com>
2022-10-12 12:29:42 +0800

    completed major functions

--- c/config.h
+++ c/config.h
@@ -1,10 +1,10 @@
-/* Configuration file for C program */
+/* Configuration file for Pulse-Width Modulation program */
 // Macros
 #define RED_LED_PIN 18          // Broadcom (BCM) GPIO pin for Red LED     (PWM Channel: 0)
 #define GREEN_LED_PIN 13        // Broadcom (BCM) GPIO pin for Green LED   (PWM Channel: 1)
 #define PWM_MAX 1000000         // Maximum value for PWM GPIO pin  (Range: 0 - 1000000)
 #define MAX_FREQ 125000000      // Maximum value for PWM Frequency (Range: 1 - 125000000)
-#define CFF_FREQ 500            // Critical Flicker Fusion Frequency (frequency at which flickering light can be perceived as continuous by the human eye)
+#define CFF_FREQ 500            // Critical Flicker Fusion Frequency (frequency at which flickering light is perceived as continuous by the human eye)
 
 // Global variables
 int BASE_RED_LED_FREQ;          // Red LED's base PWM Frequency
@@ -17,4 +17,4 @@ int getDutyCycleValue(float duty_cycle);            // Function to calculate the
 void resetLEDs();                                   // Function to reset both LEDs to their base state
 void offLED(int led_pin);                           // Function to turn off an LED
 void onLED(int led_pin);                            // Function to turn on an LED
-void blinkLED(int led_pin, int rate);               // Function to make LED blink at a specified rate
+void blinkLED(int led_pin, int freq, float dc);     // Function to make LED blink at a specified rate
--- c/pwm.c
+++ c/pwm.c
@@ -1,5 +1,5 @@
-/* Main C program for controlling PWM of the red and green LEDs. This program operates with the assumption that the Raspberry Pi and components have been set up correctly. */
-#include <pigpio.h>     // Include pigpio library, which is used to interact with the GPIO pins (Note: wiringPi not used due to its lack of support for setting separate PWM frequencies for each of the PWM channel on the Raspberry Pi)
+/* Main C program for controlling PWM of the Red and Green LEDs. This program operates with the assumption that the Raspberry Pi and its components have been set up correctly. */
+#include <pigpio.h>     // Include pigpio library, which is used to interact with the GPIO pins (Note: wiringPi is not used due to its lack of support for setting separate PWM frequencies for each of the PWM channel on the Raspberry Pi)
 #include <stdio.h>      // Include stdio library, which is used for input/output functions
 #include <stdlib.h>     // Include stdlib library, which is used for general purpose functions
 #include "config.h"     // Include configuration file
@@ -22,34 +22,59 @@ int main() {
         BASE_GREEN_LED_DC = gpioGetPWMdutycycle(GREEN_LED_PIN);       // Save Green LED's PWM Duty Cycle
     }
     
+    // Program execution message
+    printf("CSC1104 Pulse-Width Modulation Program\nThis program enables you to control the PWM of a Red and Green LED, until you have chosen to end to program.\n\n");
+
     // Do-While loop to obtain user input
     do {    
         // Get user's input
-        printf("Please enter an option from the following:\n(0) Turn off LEDs\n(1) Turn on LEDs\n(2) LEDs blink twice per second\n(3) Red LED blinks 8 times per second and Green LED blinks twice per second with reduced brightness\n(Any other number) Exit Program\n\nEnter your choice: ");
+        printf("Please select and enter an option from the following:\n(0) Turn off both LEDs\n(1) Turn on both LEDs\n(2) Make both LEDs blink twice per second\n(3) Make Red LED blink 8 times per second and Green LED blink twice per second at reduced brightness\n(Any other number) End Program\n\nEnter your choice: ");
         scanf("%d", &user_input);
 
         // Check user's input option
         switch(user_input) {
             case 0:
-                printf("Turning off LEDs!\n\n");
-                offLED(RED_LED_PIN);      // Reset LED
-                offLED(GREEN_LED_PIN);    // Reset LED
+                // Option for turning off both LEDs
+                printf("\nTurning off LEDs..\n");
+                offLED(RED_LED_PIN);      // Turn off Red LED
+                offLED(GREEN_LED_PIN);    // Turn off Green LED
+                printf("Both LEDs turned off!\n\n");
                 break;
             case 1:
-                printf("Turning on both LEDs!\n\n");
-                onLED(RED_LED_PIN);         // Turn on LED
-                onLED(GREEN_LED_PIN);       // Turn on LED
+                // Option for turning on both LEDs
+                printf("\nTurning on both LEDs..\n");
+                onLED(RED_LED_PIN);       // Turn on Red LED
+                onLED(GREEN_LED_PIN);     // Turn on Green LED
+                printf("Both LEDs turned on!\n\n");
+                break;
+            case 2:
+                // Option for making both LEDs blink twice per second
+                printf("\nMaking both LEDs blink twice per second (50%% duty cycle)..\n");
+                blinkLED(RED_LED_PIN, 2, 50);       // Set Red LED's Duty Cycle to 50% for it to be turned on half the time (square wave signal), and set Frequency to 2 for it to blink twice per second
+                blinkLED(GREEN_LED_PIN, 2, 50);     // Set Green LED's Duty Cycle to 50% for it to be turned on half the time (square wave signal), and set Frequency to 2 for it to blink twice per second
+                printf("Both LEDs blinking twice per second!\n\n");
+                break;
+            case 3:
+                // Option for making Red LED blink 8 times per second & Green LED blink twice per second at reduced brightness
+                printf("\nMaking Red LED blink 8 times per second, and Green LED blink twice per second at reduced brightness (12.5%% duty cycle)..\n");
+                blinkLED(RED_LED_PIN, 8, 50);       // Set Red LED's Duty Cycle to 50% for it to be turned on half the time (square wave signal), and set Frequency to 8 for it to blink 8 times per second
+                blinkLED(GREEN_LED_PIN, 2, 12.5);   // Set Green LED's Duty Cycle to 12.5% for it to be turned on with reduced brightness by 4 times, and set Frequency to 2 for it to blink twice per second
+                printf("Red LED is blinking 8 times per second and Green LED is blinking twice per second at reduced brightness!\n\n");
                 break;
             default:
-                printf("Program Ended!\n");
+                // Default option for any other user input values
+                // Program termination procedure
+                printf("\nProceeding to end this program..\n");
+                resetLEDs();        // Reset both LEDs
+                gpioTerminate();    // Terminate pigpio library before end of program execution
+                printf("GPIO terminated and both LEDs have been reset!\nEnd of Program!\n");
                 break;
         }
-    } while (user_input >= 0 && user_input <= 3);
 
-    // Program termination procedure
-    resetLEDs();        // Reset both LEDs
-    gpioTerminate();    // Terminate pigpio library before end of program execution
-    return 0;           // Signal successful program execution
+    } while (user_input >= 0 && user_input <= 3); // Loop continues until user chooses to end the program
+
+    // Signal successful program execution
+    return 0;
 
 }
 
@@ -79,7 +104,7 @@ void onLED(int led_pin) {
 }
 
 // Function to make LED blink at a specified rate
-void blinkLED(int led_pin, int rate){
-    // pwmSetClock(rate);
-    // pwmWrite(led_pin, 1024);
+void blinkLED(int led_pin, int freq, float dc){
+    // Set LED's frequency and duty cycle to the provided values to make it blink
+    gpioHardwarePWM(led_pin, freq, getDutyCycle(dc));
 }
